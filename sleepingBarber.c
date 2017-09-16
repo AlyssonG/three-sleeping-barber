27cc869cb94cfc24c0466334e964d8e8d936128b
AlyssonG <alyssongomes@id.uff.br>
2017-09-16 18:43:48 -0300

    Arquivo criado

---
+++ sleepingBarber.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef int semaphore;
+
+int main(void){
+	semaphore mutex;
+	semaphore barber;
+	semaphore client;
+	semaphore queue;
+	  
+	return 0;
+}
